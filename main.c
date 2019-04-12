7b3d4a18bd30762c73adb28ab16b149444fb5314
Shinpei Hayashi <hayashi@c.titech.ac.jp>
2019-04-12 12:39:42 +0900

    Fix to check the number of command line arguments

--- main.c
+++ main.c
@@ -4,6 +4,10 @@
 #include "fib.h"
 
 int main(int argc, char *argv[]) {
+    if (argc != 2) {
+        fprintf(stderr, "usage: %s n\n", argv[0]);
+        return 1;
+    }
     int n = atoi(argv[1]);
     printf("%d\n", fib(n));
     return 0;
